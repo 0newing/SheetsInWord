a53347fef3d3a5ad9bc42e05e0a75b3d9d180c2b
0newing <disciple_sneaker@163.com>
2018-09-15 10:08:53 +0800

    Demo1:Read from docx

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SheetsInWord
 transfer word to excel
+
+## Jars
+- dom4j-1.1
+- poi-3.17
+- poi-ooxml-3.17
+- poi-ooxml-schemas-3.17
+- xmlbeans-2.6.0
---
+++ src/test/Demo1.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        String buffer = "";
+        String path = "Test.docx";
+        InputStream is = null;
+        try {
+            is = new FileInputStream(path);
+            XWPFDocument doc = new XWPFDocument(is);
+            XWPFWordExtractor ex = new XWPFWordExtractor(doc);
+            buffer = ex.getText();
+            System.out.println(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                is = null;
+            }
+        }
+    }
+}
